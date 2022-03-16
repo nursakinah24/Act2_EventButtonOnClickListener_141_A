934d18f6940e6630358c7253af85d91583e5ee7e
nursakinah24 <nursakinah2402@gmail.com>
2022-03-17 01:00:28 +0700

    update new activity create menu and Floatting Button

--- app/src/main/java/com/example/activity2/MainActivity.java
+++ app/src/main/java/com/example/activity2/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.activity2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -70,5 +73,22 @@ public class MainActivity extends AppCompatActivity
                 t.show();
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.mnDaftar)
+        {
+            Intent i = new Intent(getApplicationContext(), DaftarActivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
